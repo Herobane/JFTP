99b1d9218e3fb09615d221be893a0a7fe224f2b1
Flavien <herobane1@gmail.com>
2020-01-22 09:22:18 +0100

    Added Passive Mode support

--- src/fr/herobane/jftp/ClientThread.java
+++ src/fr/herobane/jftp/ClientThread.java
@@ -1,12 +1,13 @@
 package fr.herobane.jftp;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.StringTokenizer;
 
 public class ClientThread extends Thread
 {
@@ -26,20 +27,33 @@ public class ClientThread extends Thread
 		{
 
 			Socket client = new Socket("127.0.0.1", 21);
-
-			BufferedOutputStream ostream = new BufferedOutputStream(client.getOutputStream());
-			BufferedInputStream istream = new BufferedInputStream(client.getInputStream());
-
+			
+			BufferedWriter ostream = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+			BufferedWriter dataWriter;
+			BufferedReader istream = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			BufferedReader dataReader;
+			
 			getResponse(istream);
 			sendMessage(ostream, "USER master");
 			getResponse(istream);
 			sendMessage(ostream, "PASS");
 			getResponse(istream);
-			sendMessage(ostream, "PWD");
+			sendMessage(ostream, "TYPE I");
 			getResponse(istream);
-
+			
+			Socket dataChannel = pasv(ostream, istream);
+			
+			dataWriter = new BufferedWriter(new OutputStreamWriter(dataChannel.getOutputStream()));
+			dataReader = new BufferedReader(new InputStreamReader(dataChannel.getInputStream()));
+			
+			sendMessage(ostream, "RETR test.txt");
+			getResponse(dataReader);
+			
 			ostream.close();
 			istream.close();
+			dataWriter.close();
+			dataReader.close();
+			dataChannel.close();
 			client.close();
 
 		}
@@ -58,33 +72,58 @@ public class ClientThread extends Thread
 
 	}
 
-	public String sendMessage(OutputStream ostream, String msg)
+	public Socket pasv(BufferedWriter ostream, BufferedReader istream) throws IOException
 	{
-
-		String request = msg + "\r\n";
-
-		System.out.println("[CLIENT] : " + msg);
-
-		int strLength = request.length();
+		ostream.write("PASV\r\n");
+		ostream.flush();
+		
+		String response = istream.readLine();
+		String ipAddress;
+		String[] tokens = new String[8];
+		
 		int index = 0;
-
+		int port;
+		
+		StringTokenizer tokenizer;
+		
+		System.out.println("[SERVER] : " + response);
+		
+		while (response.charAt(index) != '(')
+		{
+			index++;
+		}
+		
+		response = response.substring(index + 1, response.length() - 1);
+		
+		tokenizer = new StringTokenizer(response, ",");
+		
+		index = 0;
+		
+		while (tokenizer.hasMoreElements())
+		{
+			tokens[index] = (String) tokenizer.nextElement();
+			index++;
+		}
+		
+		ipAddress = tokens[0] + "." + tokens[1] + "." + tokens[2] + "." + tokens[3];
+		port = (Integer.parseInt(tokens[4]) * 256) + Integer.parseInt(tokens[5]);
+		
+		return new Socket(ipAddress, port);
+	}
+	
+	public String sendMessage(BufferedWriter ostream, String msg)
+	{	
+		
 		try
 		{
-
-			while (index < strLength)
-			{
-
-				ostream.write((int) request.charAt(index));
-				index++;
-
-			}
-
+			ostream.write(msg + "\r\n");
 			ostream.flush();
+			
+			System.out.println("[CLIENT] : " + msg);
 
 		}
 		catch (IOException e)
 		{
-
 			e.printStackTrace();
 
 		}
@@ -93,37 +132,40 @@ public class ClientThread extends Thread
 
 	}
 
-	public String getResponse(InputStream istream)
-	{
-
-		String response = "";
+	public void getResponse(BufferedReader istream)
+	{	
 
 		try
 		{
-//			Thread.sleep(1000);
 
-			while (istream.available() > 0)
+			Thread.sleep(1000);
+			
+			while (istream.ready())
 			{
-
-				response += Character.toString((char) istream.read());
-
+				String response = istream.readLine();
+				System.out.println("[SERVER] : " + response);
 			}
+			
+			
+			
+			// return response;
 
 		}
 		catch (IOException e)
 		{
-
 			e.printStackTrace();
+			// return "0";
 
 		}
-//		catch (InterruptedException e)
-//		{
-//			e.printStackTrace();
-//		}
+		catch (InterruptedException e)
+		{
 
-		System.out.println("[SERVER] : " + response);
+			e.printStackTrace();
+			// return "0";
+		}
+		
 
-		return response;
+		
 
 	}
 
